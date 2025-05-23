721359cac926b76bd002d63778549b455c166f6f
negativeInteger <139580532+INFERNO1211@users.noreply.github.com>
2025-05-24 00:53:05 +0530

    added html home page

--- app.js
+++ app.js
@@ -8,14 +8,44 @@ app.use(express.json());
 // Mount the school-related routes at the root path
 app.use('/', schoolRoutes);
 
-app.get('/my-ip', async (req, res) => {
-  try {
-    const response = await fetch('https://ifconfig.me/ip');
-    const ip = await response.text();
-    res.json({ outboundIP: ip });
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to get IP', details: error.message });
-  }
+app.get('/', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>School API</title>
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+          background: #f0f4f8;
+          color: #333;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          height: 100vh;
+          margin: 0;
+        }
+        h1 {
+          font-size: 3rem;
+          color: #0070f3;
+        }
+        a{
+          padding: 5px 10px;
+          background-color: skyblue;
+          color: white;
+          text-decoration: none;
+        }
+      </style>
+    </head>
+    <body>
+      <h1>Welcome to School API!</h1>
+      <h2><a href='https://github.com/negativeInteger/school-api'>Github Repository</a></h2>
+    </body>
+    </html>
+  `);
 });
 
 const PORT = process.env.PORT || 3000;
